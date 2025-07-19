eeecf44b75597a9cf0be9adf2eebb510f8b466cb
c276 <2276-91@e.mail.de>
2025-07-19 16:14:18 +0200

    improve playerhybrid

--- src/jukebox/components/playerhybrid/__init__.py
+++ src/jukebox/components/playerhybrid/__init__.py
@@ -7,201 +7,202 @@ import logging
 import components.player
 import jukebox.cfghandler
 import jukebox.plugs as plugs
+import simpleaudio
 import misc
+
 
 from .playcontentcallback import PlayContentCallbacks, PlayCardState
 
 from components.playermpd import PlayerMPD
-# from components.playerspotify import PlayerSpotify
+from components.playerspotify import PlayerSpotify
 
 logger = logging.getLogger('jb.PlayerMPD')
 cfg = jukebox.cfghandler.get_handler('jukebox')
 
+
 class PlayerHybrid:
     """Interface to Hyprid Music Player Daemon"""
 
-    
-    player_mpd = PlayerMPD()
-    # player_spotify = PlayerSpotify()
-
     def __init__(self):
-        self.player_mpd.__init__()
-        # self.player_spotify.__init__()
+        self._player_mpd = PlayerMPD()
+        self._player_spotify = PlayerSpotify()
+        # set mpd as default player
+        self.current_player = self._player_mpd
 
     def exit(self):
         logger.debug("Exit routine of players started")
-        return self.player_mpd.exit()
+        self._player_spotify.exit()
+        return self._player_mpd.exit()
 
     def connect(self):
         self.player_mpd.connect()
 
     def decode_repeat_mode(self):
-        """
-        Decodes the replay action from the configuration file and sets it accordingly.
-        """
-        self.player_mpd.decode_repeat_mode()
-        self.player_spotify.decode_repeat_mode()
+        self.current_player.decode_repeat_mode()
 
     def decode_2nd_swipe_option(self):
-        self.player_mpd.decode_2nd_swipe_option()
-        self.player_spotify.decode_2nd_swipe_option()
+        self.current_player.decode_2nd_swipe_option()
 
     @plugs.tag
     def get_player_type_and_version(self):
-        return self.player_mpd.get_player_type_and_version()
+        return self.current_player.get_player_type_and_version()
 
     @plugs.tag
     def update(self):
-        return self.player_mpd.update()
+        return self.current_player.update()
 
     @plugs.tag
     def update_wait(self):
-        return self.player_mpd.update_wait()
+        return self.current_player.update_wait()
 
     @plugs.tag
     def play(self):
-        self.player_mpd.play()
+        self.current_player.play()
 
     @plugs.tag
     def stop(self):
-        self.player_mpd.stop()
+        self.current_player.stop()
 
     @plugs.tag
     def pause(self, state: int = 1):
-        self.player_mpd.pause(state)
+        self.current_player.pause(state)
 
     @plugs.tag
     def prev(self):
-        self.player_mpd.prev()
+        self.current_player.prev()
 
     @plugs.tag
     def next(self):
-        self.player_mpd.next()
+        self.current_player.next()
 
     @plugs.tag
     def seek(self, new_time):
-        self.player_mpd.seek(new_time)
+        self.current_player.seek(new_time)
 
     @plugs.tag
     def rewind(self):
-        self.player_mpd.rewind()
+        self.current_player.rewind()
 
     @plugs.tag
     def replay(self):
-        self.player_mpd.replay()
+        self.current_player.replay()
 
     @plugs.tag
     def toggle(self):
-        self.player_mpd.toggle()
+        self.current_player.toggle()
 
     @plugs.tag
     def replay_if_stopped(self):
-        self.player_mpd.replay_if_stopped()
+        self.current_player.replay_if_stopped()
 
     @plugs.tag
     def shuffle(self, option='toggle'):
-        self.player_mpd.shuffle(option)
+        self.current_player.shuffle(option)
 
     @plugs.tag
     def repeat(self, option='toggle'):
-        self.player_mpd.repeat(option)
+        self.current_player.repeat(option)
 
     @plugs.tag
     def get_current_song(self, param):
-        return self.player_mpd.get_current_song(param)
+        return self.current_player.get_current_song(param)
 
     @plugs.tag
     def map_filename_to_playlist_pos(self, filename):
-        self.player_mpd.map_filename_to_playlist_pos(filename)
+        self.current_player.map_filename_to_playlist_pos(filename)
 
     @plugs.tag
     def remove(self):
-        self.player_mpd.remove()
+        self.current_player.remove()
 
     @plugs.tag
     def move(self):
-        self.player_mpd.move()
+        self.current_player.move()
 
     @plugs.tag
     def play_single(self, song_url):
-        self.player_mpd.play_single(song_url)
+        print(f"Playing single song: {song_url}")
+        if song_url.startswith('spotify:'):
+            self.current_player = self._player_spotify
+        else:
+            self.current_player = self._player_mpd
+        self.current_player.play_single(song_url)
+        
 
     @plugs.tag
     def resume(self):
-        self.player_mpd.resume()
+        self.current_player.resume()
 
     @plugs.tag
     def fast_forward(self, seconds: float = 1):
-        self.player_mpd.fast_forward(seconds)
+        self.current_player.fast_forward(seconds)
 
     @plugs.tag
     def play_hold_jingle(self, jingle_path: str):
-        self.player_mpd.play_hold_jingle(jingle_path)
-        # """
-        # Play a jingle while the card is held on the reader.
-        # This is used to indicate that the system is waiting for a second swipe or action.
-        # """
-        # logger.debug('Playing jingle:', jingle_path)
-        # with self.mpd_lock:
-        #     self.mpd_client.stop()
-        #     wave_obj = simpleaudio.WaveObject.from_wave_file(jingle_path)
-        #     wave_obj.play()
+        """
+        Play a jingle while the card is held on the reader.
+        This is used to indicate that the system is waiting for a second swipe or action.
+        """
+        logger.debug('Playing jingle:', jingle_path)
+        self.current_player.stop()
+        wave_obj = simpleaudio.WaveObject.from_wave_file(jingle_path)
+        wave_obj.play()
 
     @plugs.tag
     def play_card(self, folder: str, recursive: bool = False):
-        self.player_mpd.play_card(folder, recursive)
+        self.current_player.play_card(folder, recursive)
 
     @plugs.tag
     def get_single_coverart(self, song_url):
-        return self.player_mpd.get_single_coverart(song_url)
+        return self.current_player.get_single_coverart(song_url)
 
     @plugs.tag
     def get_album_coverart(self, albumartist: str, album: str):
-        return self.player_mpd.get_album_coverart(albumartist, album)
+        return self.current_player.get_album_coverart(albumartist, album)
 
     @plugs.tag
     def flush_coverart_cache(self):
-        return self.player_mpd.flush_coverart_cache()
+        return self.current_player.flush_coverart_cache()
 
     @plugs.tag
     def get_folder_content(self, folder: str):
-        return self.player_mpd.get_folder_content(folder)
+        return self.current_player.get_folder_content(folder)
 
     @plugs.tag
     def play_folder(self, folder: str, recursive: bool = False) -> None:
-        self.player_mpd.play_folder(folder, recursive)
+        self.current_player.play_folder(folder, recursive)
 
     @plugs.tag
     def play_album(self, albumartist: str, album: str):
-        self.player_mpd.play_album(albumartist, album)
+        self.current_player.play_album(albumartist, album)
 
     @plugs.tag
     def queue_load(self, folder):
-        self.player_mpd.queue_load(folder)
+        self.current_player.queue_load(folder)
 
     @plugs.tag
     def playerstatus(self):
-        return self.player_mpd.playerstatus()
+        return self.current_player.playerstatus()
 
     @plugs.tag
     def playlistinfo(self):
-        return self.player_mpd.playlistinfo()
+        return self.current_player.playlistinfo()
 
     @plugs.tag
     def list_all_dirs(self):
-        return self.player_mpd.list_all_dirs()
+        return self.current_player.list_all_dirs()
 
     @plugs.tag
     def list_albums(self):
-        return self.player_mpd.list_albums()
+        return self.current_player.list_albums()
 
     @plugs.tag
     def list_songs_by_artist_and_album(self, albumartist, album):
-        return self.player_mpd.list_songs_by_artist_and_album(albumartist, album)
+        return self.current_player.list_songs_by_artist_and_album(albumartist, album)
 
     @plugs.tag
     def get_song_by_url(self, song_url):
-        return self.player_mpd.get_song_by_url(song_url)
+        return self.current_player.get_song_by_url(song_url)
 
 # ---------------------------------------------------------------------------
 # Plugin Initializer / Finalizer
@@ -223,7 +224,7 @@ def initialize():
     plugs.register(player_ctrl, name='ctrl')
 
     global play_card_callbacks
-    play_card_callbacks = PlayContentCallbacks[PlayCardState]('play_card_callbacks', logger, context=player_ctrl.player_mpd.mpd_lock)
+    play_card_callbacks = PlayContentCallbacks[PlayCardState]('play_card_callbacks', logger, context=None)
 
     # Update mpc library
     library_update = cfg.setndefault('playerhybrid', 'library', 'update_on_startup', value=True)
