a70b77c80a41cd54666393068278e3bb46b47160
c276 <2276-91@e.mail.de>
2026-05-25 13:53:40 +0200

    fix webapp playspotify regex logic

--- src/webapp/src/components/Cards/controls/actions/play-spotify/index.js
+++ src/webapp/src/components/Cards/controls/actions/play-spotify/index.js
@@ -12,33 +12,21 @@ const SelectPlaySpotify = ({
   const values = getArgsValues(actionData);
 
   const song_url = values[0] || '';
-  const name = values[1] || '';
 
   const handleChangeUrl = (event) => {
     let newUrl = event.target.value;
     // expected_ https://open.spotify.com/playlist/04paTgaKcuPEs4eCJOKGd5?si=MHXqyuZ0TJO24zztCaIeQQ
     // apply regex pattern to extract the URI 
     // spotify:playlist:04paTgaKcuPEs4eCJOKGd5
-    const regex = /spotify:(track|album|playlist|artist):[a-zA-Z0-9]+|https:\/\/open\.spotify\.com\/(track|album|playlist|artist)\/[a-zA-Z0-9]+/;
+    const regex = /spotify:(track|album|playlist|artist):[a-zA-Z0-9]+|https:\/\/open\.spotify\.com(?:\/[^/]+)?\/(track|album|playlist|artist)\/[a-zA-Z0-9]+/g;
     const match = newUrl.match(regex);
+    let savedname = newUrl;
     // print the match    
     console.log('match', match);
     if (match) {
-      newUrl = match[0].startsWith('spotify:') ? match[0] : match[0].replace('https://open.spotify.com/', 'spotify:').replace('/', ':');
+      savedname = match[0].startsWith('spotify:') ? match[0] : match[0].replace('https://open.spotify.com/intl-de/', 'spotify:').replace('/', ':');
     }
-    if (name) {
-      newUrl = `${newUrl}#${name}`;
-    }
-    handleActionDataChange('play_spotify', 'play_spotify', { song_url: newUrl, name });
-  };
-
-  const handleChangeName = (event) => {
-    const newName = event.target.value;
-    let newUrl = song_url;
-    if (newUrl && newName) {
-      newUrl = `${newUrl.split('#')[0]}#${newName}`;
-    }
-    handleActionDataChange('play_spotify', 'play_spotify', { song_url: newUrl, name: newName });
+    handleActionDataChange('play_spotify', 'play_spotify', { song_url: savedname});
   };
 
   return (
@@ -50,15 +38,6 @@ const SelectPlaySpotify = ({
         value={song_url}
         onChange={handleChangeUrl}
         placeholder="https://open.spotify.com/playlist/123456789?si=abcde12345"
-      />
-      <TextField
-        fullWidth
-        label="Name"
-        variant="outlined"
-        value={name}
-        onChange={handleChangeName}
-        placeholder="Spotify Song Name (optional)"
-        style={{ marginTop: '0.5rem' }}
       />
     </>
   );
